5e03c9c869b302e4b7984bc4068f9d2dcdf63419
chiomaizuokwu <chigirlangel@yahoo.com>
2022-10-08 11:22:30 -0700

    Done

--- queue.c
+++ queue.c
@@ -4,20 +4,17 @@
  * @head: stack head
  * @counter: line_number
  * Return: no return
-*/
-void f_queue(stack_t **head, unsigned int counter)
+*/                                                void f_queue(stack_t **head, unsigned int counter)
 {
-	(void)head;
-	(void)counter;
-	bus.lifi = 1;
+        (void)head;
+        (void)counter;
+        bus.lifi = 1;
 }
 
 /**
  * addqueue - add node to the tail stack
- * @n: new_value
- * @head: head of the stack
- * Return: no return
-*/
+ * @n: new_value                                   * @head: head of the stack
+ * Return: no return                              */
 void addqueue(stack_t **head, int n)
 {
 	stack_t *new_node, *aux;
